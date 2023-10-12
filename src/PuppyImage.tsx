a8ee4b238623ece116c2418319575b7c8bda43ac
Nikola Todorov <s304568@student.salfordcc.ac.uk>
2023-10-12 13:47:24 +0100

    Task_1 branch

---
+++ src/PuppyImage.tsx
@@ -0,0 +1,16 @@
+import dog from "./assets/sad_dog.jpg"
+
+
+function PuppyImage() {
+
+    return (
+        <>
+    
+    <img  src={dog} alt="The dog is not here" width= "20%"/>
+        </>
+    );
+}
+
+
+
+export default PuppyImage;
